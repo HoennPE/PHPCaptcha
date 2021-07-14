e866e4bd0c83e88cab28806d8bcdaf5f530bdaef
Lyco Tatierra <lycol50@gmail.com>
2021-07-14 21:06:43 +0800

    added RWD

--- post.php
+++ post.php
@@ -47,6 +47,7 @@ curl_close($ch);
 <html lang="en">
 <head>
     <title>HoennPE Captcha Verification</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="main.css" rel="stylesheet">
     <script type='text/javascript' src="restrict.js"></script>
 </head>
